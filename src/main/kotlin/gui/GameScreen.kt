0f7ac6104d8308d12cfc47fcbe5a4f0283b999bd
Dmitry Sergeev <dmitrijsergeev111@gmail.com>
2026-05-12 23:50:59 +0300

    Add Board function

--- src/main/kotlin/gui/GameScreen.kt
+++ src/main/kotlin/gui/GameScreen.kt
@@ -1,12 +1,17 @@
 package gui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -15,6 +20,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -96,25 +102,6 @@ fun GameSidePanel(
 }
 
 @Composable
-fun MoveInput(
-    currPlayerName: String,
-    currColor: String,
-    onMove: (Int, Int, Int, String) -> Boolean,
-
-
-){
-
-
-
-}
-
-@Composable
-fun Board(board: MutableList<MutableList<Int>>){
-
-
-}
-
-@Composable
 fun GameInfoSection(
     player1: Player,
     player2: Player,
@@ -159,3 +146,51 @@ fun updateStats(game: Game, player1: Player, player2: Player, color1: String, re
     repo.update(p2)
 }
 
+@Composable
+fun MoveInput(
+    currPlayerName: String,
+    currColor: String,
+    onMove: (Int, Int, Int, String) -> Boolean,
+
+
+    ){
+
+
+
+}
+
+@Composable
+fun Board(board: MutableList<MutableList<Int>>){
+    Column {
+        for(r in 0..5 ){
+            Row{
+                for(c in 0..5){
+                    val cell = board[r][c]
+                    Box(
+                        modifier = Modifier
+                            .size(56.dp)
+                            .padding(4.dp)
+                            .border(2.dp, Color.Gray)
+                            .background(
+                                if ((r < 3 && c < 3) || (r >= 3 && c >= 3))
+                                    Color.Red else Color.Yellow
+                        ), contentAlignment = Alignment.Center
+                    ){
+                        if (cell != 0){
+                            Box(modifier = Modifier
+                                .size(40.dp)
+                                .background(
+                                    if (cell == 1) Color.White else Color.Black, CircleShape
+                                )
+                                .border(1.dp, Color.Gray, CircleShape)
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+
+}
