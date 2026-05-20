5d3fafc8984c0bbf0116eef3597671426b6f535a
Dmitry Sergeev <dmitrijsergeev111@gmail.com>
2026-05-20 20:56:29 +0300

    change game board color

--- src/main/kotlin/gui/GameScreen.kt
+++ src/main/kotlin/gui/GameScreen.kt
@@ -237,7 +237,7 @@ fun Board(board: MutableList<MutableList<Int>>) {
                             .size(56.dp)
                             .padding(4.dp)
                             .border(2.dp, Color.Gray)
-                            .background(Color.Red),
+                            .background(if ((c > 2 && r < 3) || (r > 2 && c < 3)) Color.Red else Color.Yellow),
                         contentAlignment = Alignment.Center
                     ) {
                         if (cell != 0) {
